82bab97ae14c20d2e7f784a9855dd06d32cb687b
mdalamin3091 <alamin@safewheel.zyz>
2023-12-19 22:21:21 +0600

    update mobile navbar

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import HeaderLogo from "../../assets/icons/HeaderLogo";
 import SearchIcon from "../../assets/icons/SearchIcon";
@@ -8,13 +9,16 @@ import LogoutIcon from "../../assets/icons/LogoutIcon";
 import { useAppDispatch } from "../../redux/app/hooks";
 import { logOut } from "../../redux/features/auth/authSlice";
 import { navItems } from "../../constants";
+import toast from "react-hot-toast";
 
 const Header = () => {
   const { pathname } = useLocation();
   const dispatch = useAppDispatch();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLogout = () => {
     dispatch(logOut());
+    toast.success("logout successful");
   };
 
   const handleNavActive = (navText: string): string => {
@@ -24,13 +28,26 @@ const Header = () => {
   return (
     <header className="header py-4 bg-brand-700">
       <nav className="container flex items-center justify-between">
-        <div className="flex items-center justify-between">
+        <div className="flex justify-between md:justify-start items-center w-full">
           <Link to="/">
             <HeaderLogo />
           </Link>
+
+          {/* Mobile Menu Toggle */}
+          <div className="md:hidden">
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="text-white focus:outline-none"
+            >
+              {isMenuOpen ? "Close" : "Menu"}
+            </button>
+          </div>
+
+          {/* Desktop Navigation */}
           <div className="md:flex items-center justify-between hidden gap-x-1">
             {navItems.map((navItem) => (
               <Link
+                key={navItem.link}
                 className={`nav-link ${handleNavActive(navItem.link)}`}
                 to={navItem.link}
               >
@@ -39,7 +56,36 @@ const Header = () => {
             ))}
           </div>
         </div>
-        <div className="flex items-center justify-between gap-1">
+
+        {/* Mobile Navigation */}
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } md:hidden absolute top-16 left-0 right-0 bg-brand-700 p-4 md:p-0`}
+        >
+          {navItems.map((navItem) => (
+            <Link
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              key={navItem.link}
+              className={`block text-white py-2 px-4 ${handleNavActive(
+                navItem.link
+              )}`}
+              to={navItem.link}
+            >
+              {navItem.title}
+            </Link>
+          ))}
+          <div
+            onClick={handleLogout}
+            className="flex gap-1 py-2 px-4 text-white"
+          >
+            <LogoutIcon />
+            Logout
+          </div>
+        </div>
+
+        {/* User Section */}
+        <div className="hidden md:flex items-center gap-1">
           <div className="md:flex items-center justify-center gap-1 hidden">
             <SearchIcon />
             <SettingIcon />
--- src/components/ui/TableSkeleton.tsx
+++ src/components/ui/TableSkeleton.tsx
@@ -2,15 +2,15 @@ const TableSkeleton = () => {
   return (
     <div
       role="status"
-      className="max-w-full p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+      className="max-w-full p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse"
     >
       {Array.from({ length: 10 }).map((_, idx) => (
         <div key={idx} className="flex items-center justify-between pt-4">
           <div>
-            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-            <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full w-24 mb-2.5"></div>
+            <div className="w-full h-2 bg-gray-200 rounded-full "></div>
           </div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          <div className="h-2.5 bg-gray-300 rounded-full w-12"></div>
         </div>
       ))}
     </div>
